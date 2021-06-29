0f066e50de993d3ef2d18d83b7825a2a51d7aaa0
AdarshChaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-29 11:38:21 +0100

    Create MiddleOfLinkedList.py

---
+++ linkedlist/MiddleOfLinkedList.py
@@ -0,0 +1,10 @@
+def findMid(head):
+    slow=head
+    fast=head
+    while fast and fast.next:
+        slow=slow.next
+        fast=fast.next.next
+        
+        
+    return slow.data
+        
