9ec21d0c58bc5de83e46a6dcad9efccd421a5406
AdarshChaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-07-18 11:23:16 +0100

    Update LCS.py

--- DynamicProgramming/LCS.py
+++ DynamicProgramming/LCS.py
@@ -1,13 +1,14 @@
-def lcs(x,y,n,m):
-    if n==0 or m==0:
-        return 0
-    elif x[n-1]==y[m-1]:
-        return 1+lcs(x,y,n-1,m-1)
-    else:
-        return max(lcs(x,y,n,m-1),lcs(x,y,n-1,m))
-    
 
 
+ def lcs(self,n,m,x,y):
+        dp=[[0 for j in range(m+1)]for i in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if x[i-1]==y[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[n][m]
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n=len(text1)
