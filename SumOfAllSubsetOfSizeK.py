0a10f6aa1745931aaeb6bb5fc00ed904b6117a34
AdarshChaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-09-10 12:08:00 +0530

    Update SumOfAllSubsetOfSizeK.py

--- SumOfAllSubsetOfSizeK.py
+++ SumOfAllSubsetOfSizeK.py
@@ -1,1 +1,28 @@
 
+from math import factorial as fact
+def ncr(n,r):
+    return fact(n)//(fact(r)*fact(n-r))
+
+def main():
+    arr=list(map(int,input().split()))
+    k=int(input())
+    
+    val=ncr(len(arr)-1,k-1)
+    
+    
+    print(sum(arr)*val)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+main()
+            
